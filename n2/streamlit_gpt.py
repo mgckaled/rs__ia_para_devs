7897fd4a2793659667c20b62ddf8da00d4ee62c9
mgckaled <marcelgckaled@gmail.com>
2025-03-04 23:51:54 -0300

    feat: add level-2's content

---
+++ n2/streamlit_gpt.py
@@ -0,0 +1,44 @@
+"""
+Streamlit app for interacting with OpenAI's ChatGPT model.
+
+This module provides a simple chat interface for users to interact with the ChatGPT model.
+"""
+
+import streamlit as st
+from openai import OpenAI
+
+st.title("ChatGPT Like")
+
+
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state["messages"] = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    INSTRUCTIONS = """Você é um secretário de uma clinica médica. Você atendem várias especialidades médicas.
+    Interaja com o usuário e responda normalmente as perguntas dele, quando ele quiser marcar uma consulta pergunte na ordem. 1. Qual plano de saude ele possui
+    2. Melhor data e horário para consulta. Se o plano de saúde for o Notredame, informe ao usuário que não atendemos esse plano."""
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        stream = client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            messages=[
+                {"role": "user", "content": prompt},
+                {"role": "system", "content": INSTRUCTIONS}
+            ],
+            stream=True,
+        )
+        response = st.write_stream(stream)
+    st.session_state.messages.append(
+        {"role": "assistant", "content": response})
